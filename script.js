81463f4259546ae1f897880f4231f14c5c193dc5
G Samhith <reddygsamhith@gmail.com>
2023-07-27 00:57:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+/*$('.btn').not('next').on('click',function() {
+    var correct = Math.floor(Math.random()*2+1);
+    'Correct'+correct+'.html';
+    document.body.innerHTML = "<video source src='Assests/Correct\"+corect+\".mp4' type='video/mp4' autoplay></video>"
+});*/
